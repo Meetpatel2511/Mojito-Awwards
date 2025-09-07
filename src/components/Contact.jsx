99c8690a5a08d78a4616dc9c7817b61ad31801b5
Meet Patel <meetpatel2511@gmail.com>
2025-09-07 17:15:49 +0530

    Updated hero section animation

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -65,14 +65,14 @@ const Contact = () => {
         {/* Visit section */}
         <div>
           <h3>Visit Our Bar</h3>
-          <p>456, Raq Blvd. #404, Los Angeles, CA 90210</p>
+          <p>602, Gift City. #404, Gandhinagar, GH 23120</p>
         </div>
 
         {/* Contact section */}
         <div>
           <h3>Contact Us</h3>
-          <p>(555) 987-6543</p>
-          <p>hello@jsmcocktail.com</p>
+          <p>+91 7056378912</p>
+          <p>Hey@Frontend-Developer</p>
         </div>
 
         {/* Opening hours */}
